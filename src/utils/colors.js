194938f92ae574e7ab5b9149dcd00aa4e69a117e
MasCair2k16 <gotgame6@msn.com>
2023-06-08 10:18:30 -0700

    feat: input component

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -4,11 +4,13 @@
  * blue: '#4F63AC',
  * white: '#FFFFFF',
  * orange: '#FCA340',
- * black: '#000000',}
+ * black: '#000000',
+ * grey: '#8D9BB5',}
  */
 export const colors = {
   blue: '#4F63AC',
   white: '#FFFFFF',
   orange: '#FCA340',
   black: '#000000',
+  grey: '#8D9BB5',
 };
