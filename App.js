f8f7011b4b96e9c8665d2a98aeb7f8d2c876c56e
MasCair2k16 <gotgame6@msn.com>
2023-07-08 15:15:27 -0700

    feat: tab Navigation installed

--- App.js
+++ App.js
@@ -6,9 +6,13 @@
  */
 
 import React, {useEffect} from 'react';
-// import {View, SafeAreaView} from 'react-native';
+import {Image} from 'react-native';
 import Splash from './src/screens/auth/Splash';
 import Signup from './src/screens/auth/Signup';
+import Home from './src/screens/app/Home';
+import Favorites from './src/screens/app/Favorites';
+import Profile from './src/screens/app/Profile';
+// import Settings from './src/screens/app/Settings';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import Config from 'react-native-config';
 import Signin from './src/screens/auth/Signin';
@@ -16,8 +20,49 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {colors} from './src/utils/colors';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// ! Fix this later
+const Tabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused}) => {
+          let icon;
+          if (route.name === 'Home') {
+            icon = focused
+              ? require('./src/assets/tabs/home_active.png')
+              : require('./src/assets/tabs/home.png');
+          } else if (route.name === 'Profile') {
+            icon = focused
+              ? require('./src/assets/tabs/profile_active.png')
+              : require('./src/assets/tabs/profile.png');
+          } else if (route.name === 'Favorites') {
+            icon = focused
+              ? require('./src/assets/tabs/bookmark_active.png')
+              : require('./src/assets/tabs/bookmark.png');
+          }
+          // You can return any component that you like here!
+          return <Image style={{width: 24, height: 24}} source={icon} />;
+        },
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {borderTopColor: colors.white},
+      })}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Favorites" component={Favorites} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+};
 
 const App = () => {
+  const isSignedIn = true;
+
+  // initial start of app running
   useEffect(() => {
     GoogleSignin.configure({
       scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
@@ -27,8 +72,7 @@ const App = () => {
     });
   }, []);
 
-  const Stack = createNativeStackNavigator();
-
+  // 
   const theme = {
     colors: {
       background: colors.white,
@@ -39,21 +83,36 @@ const App = () => {
     <SafeAreaProvider>
       <NavigationContainer theme={theme}>
         <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={Splash}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Signin"
-            component={Signin}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Signup"
-            component={Signup}
-            options={{headerShown: false}}
-          />
+          {/* When user is signed in */}
+          {isSignedIn && (
+            <>
+              <Stack.Screen
+                name="Tabs"
+                component={Tabs}
+                options={{headerShown: false}}
+              />
+            </>
+          )}
+          {/* When user is not signed in */}
+          {!isSignedIn && (
+            <>
+              <Stack.Screen
+                name="Splash"
+                component={Splash}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen
+                name="Signin"
+                component={Signin}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen
+                name="Signup"
+                component={Signup}
+                options={{headerShown: false}}
+              />
+            </>
+          )}
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
